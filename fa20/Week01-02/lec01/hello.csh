fa2bb6cf9d951d7740196dd4bf969a19fd569080
Yury <ygkolomensky@lbl.gov>
2020-08-26 22:56:29 -0700

    Week 1-2 for SP20 semesters. A few mods, but mostly checkpoints for the previous semesters

---
+++ fa20/Week01-02/lec01/hello.csh
@@ -0,0 +1,7 @@
+#!/bin/csh -f
+
+echo "Hello world!"
+
+echo "Hellow again"
+
+echo "and again"
---
+++ fa20/Week01-02/lec01/test.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.array([1,2,3,4])
+y = x**2
+plt.plot(x,y,"r-")
+plt.show()
